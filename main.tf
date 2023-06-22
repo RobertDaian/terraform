65091abc86b4f4fac50a33b43e50787679e8736e
RobertDaian <robert.daian1981@gmail.com>
2023-06-22 14:56:40 -0300

    enviando os arquivos para o github

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+    version = "~>2.0"
+    region = "us-east-1"
+
+}
+
+resource "aws_instance" "dev" {
+    count = 3
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
+        tags = {
+      Name = "dev${count.index}"
+    }
+    vpc_security_group_ids = ["sg-0c1e9204fe47ef1f5"]
+}
+
+resource "aws_security_group" "acesso-ssh"{
+    name =        "acesso-ssh"
+    description = "acesso-ssh"
+
+    ingress {
+        from_port = 22
+        to_port   = 22
+        protocol = "tcp"
+
+    }
+    tags = {
+        name = "ssh"
+    }
+}
+
+#teste1

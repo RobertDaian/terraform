e26e726756d9b161dec51bb84446e77687c87dc5
RobertDaian <robert.daian1981@gmail.com>
2023-06-23 14:01:39 -0300

    D

--- main.tf
+++ main.tf
@@ -12,7 +12,28 @@ resource "aws_instance" "dev" {
         tags = {
       Name = "dev${count.index}"
     }
-    vpc_security_group_ids = ["sg-0c1e9204fe47ef1f5"]
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+resource "aws_instance" "dev4" {
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
+        tags = {
+      Name = "dev4"
+    }
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+    depends_on = [ aws_s3_bucket.dev4 ]
+}
+
+resource "aws_instance" "dev5" {
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
+        tags = {
+      Name = "dev5"
+    }
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
 }
 
 resource "aws_security_group" "acesso-ssh"{
@@ -30,4 +51,12 @@ resource "aws_security_group" "acesso-ssh"{
     }
 }
 
-#teste1
+resource "aws_s3_bucket" "dev4" {
+  bucket = "mikeohearn-dev4"
+
+  tags = {
+    Name = "mikeohearn-dev4"
+    
+  }
+}
+

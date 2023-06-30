ad7a0e2b458535e644a652dd5351e3bc9ce49593
RobertDaian <robert.daian1981@gmail.com>
2023-06-30 14:03:24 -0300

    Adicionando o script de outputs

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+output "dev" {
+    
+    value = "${aws_instance.dev[*].public_ip}"
+    }
+
+output "dev5" {
+    value = "${aws_instance.dev5.public_ip}"
+    }
+
+output "dev6" {
+    value = "${aws_instance.dev6.public_ip}"
+    }    
+
+output "dev7" {
+    value = "${aws_instance.dev7.public_ip}"
+    }
+

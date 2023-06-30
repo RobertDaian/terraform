dbe095df459ca3380aedccfae5578bdecb1a2e05
RobertDaian <robert.daian1981@gmail.com>
2023-06-30 10:02:17 -0300

    Apagando o bucket s3 e suas dependencias

---
+++ vars.tf
@@ -0,0 +1,22 @@
+variable "amis" {
+  type = map(string)
+
+  default = {
+    "us-east-1" = "ami-053b0d53c279acc90"
+    "us-east-2" = "ami-024e6efaf93d85776"
+  }
+}
+
+variable "cdirs_acesso_remoto" {
+    type = list(string)
+    default = ["189.102.35.84/32","189.102.35.84/32"]
+
+    
+  
+}
+
+variable "key_name" {
+
+  default = "terraform-aws"
+  
+}
